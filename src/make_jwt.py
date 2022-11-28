78cad489dd4d2a8e45a41f9daa90223bffd6d7e0
Guillaume Genty <ggenty@waycom.net>
2022-11-28 16:18:26 +0100

    Better arguments names

--- src/make_jwt.py
+++ src/make_jwt.py
@@ -11,9 +11,9 @@ from keyring import private_key, kid, issuer
 def main():
     parser = argparse.ArgumentParser(description="Easy JWKS, Token maker")
     parser.add_argument("subject", type=str, help="Subject (sub) of the token")
-    parser.add_argument("--audience", type=str, nargs='*', help="Audiences (aud) of the token", default=[issuer])
-    parser.add_argument("--valid-before", type=int, help="Seconds of validity in the past (default: 0)", default=0)
-    parser.add_argument("--seconds", type=int, help="Seconds of validity after issue  (default: not used)", default=0)
+    parser.add_argument("--audience", type=str, nargs="+", help="Audiences (aud) of the token", default=[issuer])
+    parser.add_argument("--seconds-before", type=int, help="Seconds of validity in the past (default: 0)", default=0)
+    parser.add_argument("--seconds", type=int, help="Seconds of validity after issue  (default: not used, see --days)", default=0)
     parser.add_argument("--days", type=int, help="Days of validity after issue  (default: 1)", default=1)
     args = parser.parse_args()
 
@@ -23,7 +23,7 @@ def main():
         "iss": issuer,
         "aud": args.audience,
         "iat": now,
-        "nbf": now - datetime.timedelta(seconds=args.valid_before),
+        "nbf": now - datetime.timedelta(seconds=args.seconds_before),
         "exp": now + datetime.timedelta(seconds=args.seconds, days=args.days),
         "sub": args.subject
     }
